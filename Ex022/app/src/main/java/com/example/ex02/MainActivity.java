e0f57a74eb393af68a583ae750ed8deb85a5a589
gbox3d <gbox3d@gmail.com>
2024-10-08 12:38:41 +0900

    first commit

---
+++ Ex022/app/src/main/java/com/example/ex02/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.ex02;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    Button btnSend;
+    EditText etMsg;
+    TextView tvMsg;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        btnSend = findViewById(R.id.btnSend);
+        etMsg = findViewById(R.id.etMsg);
+        tvMsg = findViewById(R.id.tvMsg);
+
+        btnSend.setOnClickListener(v -> {
+            String msg = etMsg.getText().toString();
+            tvMsg.setText(msg);
+        });
+    }
+}
