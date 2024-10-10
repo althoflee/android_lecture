29ed732e771b699fad6d50e45ed45ca79b6b55fa
althoflee <gbox3dgel3@gmail.com>
2024-10-10 10:29:12 +0900

    ....

---
+++ Hello/app/src/main/java/com/example/hello/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.hello;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Log.v("MainActivity", "onCreate");
+
+        TextView txHello = findViewById(R.id.txHello);
+        //txHello.setText("Hi Jeonju!");
+
+        Button btnHello = findViewById(R.id.btnTest);
+
+        btnHello.setOnClickListener(v -> {
+            txHello.setText("Hi Jeonju!");
+        });
+
+
+    }
+}
